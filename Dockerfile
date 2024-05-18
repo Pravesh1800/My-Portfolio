36ed2d184a349da56fc5257eb7dfc1b75dc0e40f
Pravesh Singh <praveshds1800@gmail.com>
2024-05-18 18:03:12 +0530

    Fixing dependency errors

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,28 @@
 # Use the official Python image from the Docker Hub
-FROM python:3.9
+FROM python:3.11-slim
+
+ENV PYTHONUNBUFFERED = 1
 
 # Set the working directory
 WORKDIR /app
 
-# Copy the requirements file
-COPY requirements.txt .
+RUN python -m pip install --upgrade pip
+RUN pip install virtualenv
+RUN python -m venv .venv
+RUN . .venv/bin/activate
 
-# Install the dependencies
-RUN pip install -r requirements.txt
+COPY . .
+
+RUN --mount=type=cache,target=/root/.cache/pip \
+    --mount=type=bind,source=requirements.txt,target=requirements.txt \
+    python -m pip install -r requirements.txt
+
 
 # Copy the rest of the application code
-COPY . .
 
 # Expose the port Streamlit will run on
 EXPOSE 8501
 
 # Command to run the application
-CMD ["streamlit", "run", "About-Me.py", "--server.port=8501", "--server.enableCORS=false"]
+CMD ["streamlit", "run", "test.py", "--server.port=8501", "--server.enableCORS=false"]
+
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+st.set_page_config(page_title="My Portfolio", page_icon="👨", layout="wide")
+
+
 with st.sidebar:
     selected = option_menu('My Digital Portfolio',
 
@@ -323,46 +326,57 @@ if selected == "☠️ Suicidal Detection":
     from tensorflow.keras.models import load_model
     from tensorflow.keras.preprocessing.text import one_hot
     from tensorflow.keras.preprocessing.sequence import pad_sequences
-    import nltk
+    from pathlib import Path
     import re
+    import zipfile
     from nltk.corpus import stopwords
     from nltk.stem.porter import PorterStemmer
     ps = PorterStemmer()
 
-    model = load_model("Sucidal_classification/Sucidal_classification.keras")
+
+    st.write(zipfile.is_zipfile(
+        "Sucidal_classification/Sucidal_classification.keras"
+    ))
+
+    
+    if Path("Sucidal_classification/Sucidal_classification.keras").exists() == False :
+        st.write("Model might be downloading, please wait.....")
+    
+    else:
+        model = load_model("Sucidal_classification/Sucidal_classification.keras")
 
 
-    st.title("Sucidal text classification")
-    st.image('Sucidal_classification\img.jpg',use_column_width=True)
-    st.write("Please enter the text you want to analyze in the text box and you will get the result as suicidal if the text has suicidal tendency or non suicidal if the text has non-suicidal tendency")
+        st.title("Sucidal text classification")
+        st.image('Sucidal_classification/img.jpg',use_column_width=True)
+        st.write("Please enter the text you want to analyze in the text box and you will get the result as suicidal if the text has suicidal tendency or non suicidal if the text has non-suicidal tendency")
 
-    text = st.text_input("Enter the text you want to check")
-
-
-    voc_size = 5000
-    sent_length = 70
+        text = st.text_input("Enter the text you want to check")
 
 
-    sen = re.sub('[^a-zA-Z]',' ', text)
-    sen = sen.lower()
-    sen = sen.split()
-    sen = [ps.stem(word) for word in sen if not word in stopwords.words('english')]
-    sen = ' '.join(sen)
-    encoded_sen = [one_hot(sen,voc_size)]
-    embeded_sen = pad_sequences(encoded_sen,padding='post',maxlen = sent_length)
+        voc_size = 5000
+        sent_length = 70
 
-    pred = model.predict(embeded_sen)
-    posibility = pred.round(4)
 
-    if text :
-        if posibility<0.5:
-            st.info("This text does not have a sucidal tendency.")
-            st.image('Sucidal_classification/non-sucidal.gif', use_column_width=True)
+        sen = re.sub('[^a-zA-Z]',' ', text)
+        sen = sen.lower()
+        sen = sen.split()
+        sen = [ps.stem(word) for word in sen if not word in stopwords.words('english')]
+        sen = ' '.join(sen)
+        encoded_sen = [one_hot(sen,voc_size)]
+        embeded_sen = pad_sequences(encoded_sen,padding='post',maxlen = sent_length)
 
-        else:
-            st.warning("This text may have sucidal tendency.")
-            st.image('Sucidal_classification\sucidal.gif', use_column_width=True)
-            st.write(posibility)
+        pred = model.predict(embeded_sen)
+        posibility = pred.round(4)
+
+        if text :
+            if posibility<0.5:
+                st.info("This text does not have a sucidal tendency.")
+                st.image('Sucidal_classification/non-sucidal.gif', use_column_width=True)
+
+            else:
+                st.warning("This text may have sucidal tendency.")
+                st.image('Sucidal_classification\sucidal.gif', use_column_width=True)
+                st.write(posibility)
 
 
 #======================================================================================== Image Classification ============================================================================================================================    
